09979f4d76ae584c5f38f3a2a03a0f494c1b585b
MohamedAyoubJs <113200512+Mohmed-Ayoub-Js@users.noreply.github.com>
2024-08-11 15:47:40 +0200

    Add files via upload

---
+++ app/[surah]/_components/tafseer-component.tsx
@@ -0,0 +1,117 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { TafseerListType, TafseerType } from "@/types";
+import { BookIcon, Loader2 } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useState, useTransition } from "react";
+import { GetTafseer } from "@/action";
+import { toast } from "sonner";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+type TafseerComponentProps = {
+  surahNumber: number;
+  ayahNumber: number;
+  TafseerList: TafseerListType;
+};
+
+export default function TafseerComponent({
+  surahNumber,
+  ayahNumber,
+  TafseerList,
+}: TafseerComponentProps) {
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [isPending, startTransition] = useTransition();
+  const [tafseerData, setTafseerData] = useState<TafseerType | undefined>(
+    undefined
+  );
+
+  const handleTafseerClick = (tafseerId: number) => {
+    startTransition(() => {
+      setIsDialogOpen(true);
+      GetTafseer({
+        surahNumber,
+        ayahNumber,
+        tafseerId,
+      })
+        .then((res) => {
+          if (res.status === 200) {
+            setTafseerData(res.data);
+          } else {
+            toast.error("Failed to get tafseer");
+          }
+        })
+        .catch((err) => {
+          toast.error("Failed to get tafseer");
+        });
+    });
+  };
+
+  return (
+    <>
+      {/* Mofasir Dropdown */}
+      <DropdownMenu>
+        <Button asChild variant={"outline"} className="size-9 p-0.5">
+          <DropdownMenuTrigger>
+            <BookIcon className="size-4" />
+          </DropdownMenuTrigger>
+        </Button>
+        <DropdownMenuContent className="font-kurdish text-right">
+          <DropdownMenuLabel>تفسير</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          {TafseerList.map((tafseer) => (
+            <DropdownMenuItem
+              className="justify-end"
+              key={tafseer.id}
+              onClick={() => {
+                handleTafseerClick(tafseer.id);
+              }}>
+              {tafseer.name}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      {/* Tafseer Dialog */}
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <DialogContent className="font-kurdish">
+          {isPending ? (
+            <Loading />
+          ) : (
+            <>
+              <DialogHeader>
+                <DialogTitle className="text-center">
+                  {tafseerData?.tafseer_name}
+                </DialogTitle>
+              </DialogHeader>
+              <ScrollArea className="max-h-[80vh] p-4 text-justify rtl">
+                : <p>{tafseerData?.text}</p>
+              </ScrollArea>
+            </>
+          )}
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+}
+
+function Loading() {
+  return (
+    <div className="grid place-items-center min-h-48">
+      <Loader2 className="size-4" />{" "}
+    </div>
+  );
+}
--- app/[surah]/page.tsx
+++ app/[surah]/page.tsx
@@ -2,15 +2,19 @@ import SurahComponent from "./_components/surah-component";
 import SomethingWentWrong from "@/components/something-went-wrong";
 import { GetSurah, GetTafseerList } from "@/action";
 import { redirect } from "next/navigation";
-
 export default async function Page({ params }: { params: { surah: string } }) {
   // check if the params value is a number
   const surahNumber = Number(params.surah);
   if (isNaN(surahNumber)) redirect("/");
-
+  const { error: tafseerError, data: TafseerList } = await GetTafseerList();
+  if (tafseerError || !TafseerList) return <SomethingWentWrong />;
   // get surah data
   const { error, data } = await GetSurah({ surahNumber });
   if (error || !data) return <SomethingWentWrong />;
 
-  return <div className="py-8">{<SurahComponent surah={data} />}</div>;
+  return (
+    <div className="py-8">
+      {<SurahComponent surah={data} TafseerList={TafseerList} />}
+    </div>
+  )
 }
