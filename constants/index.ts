7d1bbf193c425166d8bff78a7ddbe3194519c424
HoshangDEV <hoshangfy@gmail.com>
2025-02-23 12:03:34 +0300

    refactor: reorder font names in constants

--- constants/index.ts
+++ constants/index.ts
@@ -1,26 +1,26 @@
 export const FontNames = [
   {
-    label: "Noto Kufi Arabic",
-    value: "noto-kufi-arabic",
+    label: "Amiri Quran",
+    value: "amiri-quran",
   },
   {
     label: "Amiri",
     value: "amiri",
   },
   {
-    label: "Amiri Quran",
-    value: "amiri-quran",
-  },
-  {
-    label: "Baloo Bhaijaan 2",
-    value: "baloo-bhaijaan-2",
+    label: "Scheherazade New",
+    value: "scheherazade-new",
   },
   {
     label: "Noto Naskh Arabic",
     value: "noto-naskh-arabic",
   },
   {
-    label: "Scheherazade New",
-    value: "scheherazade-new",
+    label: "Baloo Bhaijaan 2",
+    value: "baloo-bhaijaan-2",
+  },
+  {
+    label: "Noto Kufi Arabic",
+    value: "noto-kufi-arabic",
   },
 ];
